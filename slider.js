d7908b7518adde25d37677a6d0aa43b20da2d9c9
Alexandra Shibina <alexandra.shibina@gmail.com>
2020-08-23 10:36:21 +0300

    slider fixed + blocks added

--- slider.js
+++ slider.js
@@ -1,32 +1,57 @@
-const computedStyle = window.getComputedStyle(items);
-const leftBtn = document.querySelector("#left");
-const rightBtn = document.querySelector("#right");
-const items = document.querySelector("#items");
+const left = document.querySelector("#left");
+const right = document.querySelector("#right");
+const itemsList = document.querySelector("#items");
+const items = document.querySelectorAll(".item");
+const computedStyles = window.getComputedStyle(itemsList);
 
 const minRight = 0;
 const itemWidth = getComputedStyle(items[0]).width;
 const step = parseInt(itemWidth);
-const preShownItems = 300 / step //container width - size of the element
-const maxRight = (items.legnth - preShownItems) * step;
+const preShownItems = 300 / step; //container width - size of the element
+const maxRight = (items.length - preShownItems) * step;
 let currentRight = 0;
 
-items.style.right = currentRight;
+itemsList.style.right = currentRight;
 
-right.addEventListener("click", (e) => {
+right.addEventListener("click", e => {
     e.preventDefault();
 
     if (currentRight < maxRight) {
         currentRight += step;
-        items.style.right = `${currentRight}`;
+        itemsList.style.right = `${currentRight}px`;
     }
 })
 
-left.addEventListener("click", (e) => {
+left.addEventListener("click", e => {
     e.preventDefault();
 
-    if (currentRight < minRight) {
+    if (currentRight > minRight) {
         currentRight -= step;
-        items.style.right = `${currentRight}`;
+        itemsList.style.right = `${currentRight}px`;
     }
 
 })
+
+// const left = document.querySelector("#left");
+// const right = document.querySelector("#right");
+// const items = document.querySelector("#items");
+// const computed = window.getComputedStyle(items);
+
+// right.addEventListener("click", function(e) {
+//     e.preventDefault();
+//     let currentRight = parseInt(computed.right);
+
+//     if (currentRight < 500) {
+//         items.style.right = currentRight + 100 + "px";
+//     }
+// })
+
+// left.addEventListener("click", function(e) {
+//     e.preventDefault();
+//     let currentRight = parseInt(computed.right);
+
+//     if (currentRight > 0) {
+//         items.style.right = currentRight - 100 + "px";
+//     }
+
+// })
